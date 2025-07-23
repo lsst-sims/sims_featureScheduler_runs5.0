63e193175e7730ccc3b90e5f3de2f3c5fc33c7ef
Peter Yoachim <yoachim@uw.edu>
2025-07-23 16:49:35 -0700

    all dither

---
+++ ddf_all_dither/README.md
@@ -0,0 +1,2 @@
+Same as DDF_dither, but turn on every visits dithering for DDFs
+
---
+++ ddf_all_dither/ddf_all_dither.sh
@@ -0,0 +1,5 @@
+python ddf_all_dither.py --max_dither .01
+python ddf_all_dither.py --max_dither .1
+python ddf_all_dither.py --max_dither .2
+python ddf_all_dither.py --max_dither .4
+python ddf_all_dither.py --max_dither .8
